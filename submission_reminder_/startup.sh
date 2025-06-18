40945d7cdea16308347a2a6873ceecc388684786
divine150-coder <d.mutesi1@alustudent.com>
2025-06-18 04:08:03 -0700

    remove unwanted directories.

--- submission_reminder_/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the directory where this script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Start the reminder application
-echo "Starting submission reminder app..."
-"$SCRIPT_DIR/app/reminder.sh"
